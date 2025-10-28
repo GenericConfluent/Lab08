d86c77dde0e39e092e1e651a7c884931239982f9
GenericConfluent <m13power@gmail.com>
2025-10-28 16:02:38 -0600

    test: hasCity checks

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -3,5 +3,20 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
+    @Test
+    public void testHasCity() {
+        CustomList customList = new CustomList();
+        City edmonton = new City("Edmonton", "AB");
+        City calgary = new City("Calgary", "AB");
+        City vancouver = new City("Vancouver", "BC");
+        City toronto = new City("Toronto", "ON");
 
+        customList.addCity(edmonton);
+        customList.addCity(calgary);
+
+        assert customList.hasCity(edmonton);
+        assert customList.hasCity(calgary);
+        assert !customList.hasCity(vancouver);
+        assert !customList.hasCity(toronto);
+    }
 }
